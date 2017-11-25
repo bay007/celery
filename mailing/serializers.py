7352c9a4ac021c47f6ea5d6bd63e94a5866765f2
Egaliciar <tnt_code@hotmail.com>
2017-11-24 21:50:25 -0600

    mandar un email al insertar un usuario en la DB

--- mailing/serializers.py
+++ mailing/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from mailing.models import mailing
-
+from mailing.tasks import sendEmail
 
 # Se debe especificar que es de tipo MODEL, si no la vista retornara vacio
 
@@ -9,3 +9,9 @@ class MailingSerializer(serializers.ModelSerializer):
     class Meta:
         model = mailing
         fields = ('__all__')
+
+    def create(self, validated_data):
+
+        sendEmail(
+            validated_data["email"], validated_data["firstname"], validated_data["lastname"])
+        return mailing(**validated_data)
